75a6e4c9aadaf5c4b8c370dee473bcf792e6ad5e
onlyeat3 <>
2020-06-22 18:01:29 +0800

    Initial commit

---
+++ app/src/main/java/io/github/onlyeat3/zero_to_finish/ScrollViewActivity.java
@@ -0,0 +1,14 @@
+package io.github.onlyeat3.zero_to_finish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScrollViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view);
+    }
+}
